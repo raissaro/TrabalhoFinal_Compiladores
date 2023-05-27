f690c7dec1034433bcbf72530277b27c74d1853c
Raissa <raissaolivfac@gmail.com>
2023-05-26 23:18:17 -0300

    Final

--- src/ExemploLexer.java
+++ src/ExemploLexer.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.Token;
 
@@ -9,12 +10,12 @@ public class ExemploLexer {
             org.antlr.v4.runtime.CharStream input = CharStreams.fromFileName(filename);
             MinhaGramaticaLexer lexer = new MinhaGramaticaLexer(input);
             Token token;
+            
             while(!lexer._hitEOF){
                 token = lexer.nextToken();
                 System.out.println("Token: "+ token.toString());
                 System.out.println("    Lexema: " + token.getText());
                 System.out.println("    Classe: " + lexer.getVocabulary().getDisplayName(token.getType()));
-         
             }
 
         }catch(IOException e){
